e4210f673a6921f4f8808facd4a61e861638153d
Dominic LoBue <dominic.lobue@gmail.com>
2012-05-16 21:30:52 -0700

    initial commit -- rough extraction from original cookbook

---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+
+package "python-powerupdater"
+
+cron "powerupdater" do
+    action(:delete) unless node.mongodb.ismaster
+    minute "*/2"
+    command "powerupdater 2>&1 | /usr/bin/logger -t powerupdater"
+end
+
